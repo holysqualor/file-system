2ca90cbf8fe5343752239f4b02624d80cfced5d0
holysqualor <artmkokoko@gmail.com>
2026-03-04 17:42:13 +0200

    Create first objects

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
+#include "FSObject.h"
+#include "SpaceManager.h"
+#include "Journal.h"
+
 using namespace std;
 
 int main() {
-    cout << "Hello world!" << endl;
+    FSObject file("file.txt", false);
+    SpaceManager sm(512);
+    Journal journal;
     return 0;
 }
